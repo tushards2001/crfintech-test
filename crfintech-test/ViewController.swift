82458328cdad886397ef0581363f48bcced2154d
Milind Satam <milind.ms14@gmail.com>
2017-12-21 12:23:39 +0530

    Initial Commit

---
+++ crfintech-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crfintech-test
+//
+//  Created by MacBookPro on 12/21/17.
+//  Copyright © 2017 basicdas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
